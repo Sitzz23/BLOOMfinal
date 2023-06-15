24c1e48c0438dd3fa6bb73e98fb9e913d36ed5d4
i-haq786 <i.haqcs@gmail.com>
2023-06-15 11:23:08 +0530

    WIP Card Done

--- BLOOMFINAL/View/Main/EventCardView.swift
+++ BLOOMFINAL/View/Main/EventCardView.swift
@@ -16,16 +16,40 @@ struct EventCardView: View {
     
     var body: some View {
         GeometryReader {
+            let registeredUsers : Int = 150
             let size = $0.size
-            let rect = $0.frame(in: .named("SCROLLVIEW"))
-            //        VStack(alignment: .leading){
-            //            VStack(spacing: 10) {
+            let _rect = $0.frame(in: .named("SCROLLVIEW"))
+            
             HStack(spacing: -25){
                 VStack(alignment: .center, spacing: 6){
                                     VStack(alignment: .leading){
                                         Text(event.name)
                                             .font(.title3)
                                             .fontWeight(.semibold)
+                                        
+                                        Text(event.date.formatted(date: .numeric, time: .shortened))
+                                            .font(.caption)
+                                            .foregroundColor(Color("primary"))
+                                        
+                                        Spacer(minLength: 10)
+                                        
+                                        HStack(spacing: 4){
+                                            
+                                            Text("\(registeredUsers)")
+                                                .font(.caption)
+                                                .fontWeight(.semibold)
+                                                .foregroundColor(Color("accent"))
+                                            
+                                            Text("Registrations")
+                                                .font(.caption)
+                                                .foregroundColor(Color("primary"))
+                                            
+                                            Spacer(minLength: 0)
+                                            
+                                            Image(systemName: "chevron.right")
+                                                .font(.caption)
+                                                .foregroundColor(Color("primary"))
+                                        }
 //                                        HStack{
 //                                            Image(systemName: "location")
 //                                                .font(.system(size: 16))
@@ -39,23 +63,17 @@ struct EventCardView: View {
 //                                            Image(systemName: "calendar")
 //                                                .font(.system(size: 16))
 //                                                .foregroundColor(Color("accent"))
-                                            Text(event.date.formatted(date: .numeric, time: .shortened))
-                                                .font(.caption)
-                                                .foregroundColor(Color("primary"))
 //                                        }
-                    
 //                                        HStack{
 //                                            Image(systemName: "text.bubble")
 //                                                .font(.system(size: 16))
 //                                                .foregroundColor(Color("accent"))
-                                            Text(event.description)
-                                                .textSelection(.enabled)
-                                                .font(.caption2)
-                                                .padding(.vertical,8)
-                    
+//                                            Text(event.description)
+//                                                .textSelection(.enabled)
+//                                                .font(.caption2)
+//                                                .padding(.vertical,8)
 //                                        }
-                    
-                    
+
                                     }.padding()
                 }
                 .frame(width: size.width/2, height: size.height * 0.8)
@@ -67,22 +85,8 @@ struct EventCardView: View {
                 }
                 .zIndex(1)
                 
-               // .
-                //                        VStack(alignment: .leading, spacing: 6){
-                //                            Text("Hieiie")
-                //                        }
-                //                        .frame(width: size.width/2, height: size.height * 0.8)
-                //                        .background{
-                //                            RoundedRectangle(cornerRadius: 10, style: .continuous)
-                //                                .fill(.white)
-                //                                .shadow(color: .black.opacity(0.08), radius: 8, x: 5, y: 5)
-                //                                .shadow(color: .black.opacity(0.08), radius: 8, x: -5, y: -5)
-                //                        }
-                
                 ZStack{
                     if let eventImage = event.imgURL{
-                        //                                GeometryReader{
-                        //                                    let size1 = $0.size
                         WebImage(url: eventImage)
                             .resizable ()
                             .aspectRatio (contentMode: .fill)
@@ -98,8 +102,5 @@ struct EventCardView: View {
                         
                 }
             }
-               // .hAlign(.leading)
-         
-//    }
-//}
+
 
